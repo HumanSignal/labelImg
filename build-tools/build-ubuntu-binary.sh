c95d51c9545a0be3b9e90df7afec85d39a77f98c
Arnav Kohli <arnavkohli@gmail.com>
2016-12-07 10:51:50 +0800

    Add envsetup.sh build-window-binary.sh for building windows 32 binary

--- build-tools/build-ubuntu-binary.sh
+++ build-tools/build-ubuntu-binary.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-pyinstaller --hidden-import=xml \
+
+python pyinstaller/pyinstaller.py --hidden-import=xml \
             --hidden-import=xml.etree \
             --hidden-import=xml.etree.ElementTree \
             --hidden-import=lxml.etree \
             --hidden-import=json \
-            --hidden-import=numpy \
             --hidden-import=numpy \
              -D -F -n labelImg -c "../labelImg.py" -p ../libs
