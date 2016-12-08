dbbd6fc0f7d8c3e3b59f1f24c1ad78765ebdfb0a
tzutalin <tzu.ta.lin@gmail.com>
2016-12-08 10:09:42 +0800

    Update scripts for releasing

--- build-tools/build-ubuntu-binary.sh
+++ build-tools/build-ubuntu-binary.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+### Ubuntu use pyinstall v3.0
+THIS_SCRIPT_PATH=`readlink -f $0`
+THIS_SCRIPT_DIR=`dirname ${THIS_SCRIPT_PATH}`
+cd pyinstaller
+git checkout v3.0
+cd ${THIS_SCRIPT_DIR}
+
 rm -r build
 rm -r dist
 rm labelImg.spec
@@ -6,6 +13,12 @@ python pyinstaller/pyinstaller.py --hidden-import=xml \
             --hidden-import=xml.etree \
             --hidden-import=xml.etree.ElementTree \
             --hidden-import=lxml.etree \
-            --hidden-import=json \
-            --hidden-import=numpy \
              -D -F -n labelImg -c "../labelImg.py" -p ../libs
+
+FOLDER=$(git describe --abbrev=0 --tags)
+FOLDER="linux_"$FOLDER
+rm -rf "$FOLDER"
+mkdir "$FOLDER"
+cp dist/labelImg $FOLDER
+cp -rf ../data $FOLDER/data
+zip "$FOLDER.zip" -r $FOLDER
--- build-tools/build-windows-binary.sh
+++ build-tools/build-windows-binary.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+### Window requires pyinstall v2.1
+THIS_SCRIPT_PATH=`readlink -f $0`
+THIS_SCRIPT_DIR=`dirname ${THIS_SCRIPT_PATH}`
+cd pyinstaller
+git checkout v2.1
+cd ${THIS_SCRIPT_DIR}
+
 . venv_wine/bin/activate
 rm -r build
 rm -r dist
@@ -7,6 +14,12 @@ wine c:/Python27/python.exe pyinstaller/pyinstaller.py --hidden-import=xml \
             --hidden-import=xml.etree \
             --hidden-import=xml.etree.ElementTree \
             --hidden-import=lxml.etree \
-            --hidden-import=json \
-            --hidden-import=numpy \
              -D -F -n labelImg -c "../labelImg.py" -p ../libs
+
+FOLDER=$(git describe --abbrev=0 --tags)
+FOLDER="windows_"$FOLDER
+rm -rf "$FOLDER"
+mkdir "$FOLDER"
+cp dist/labelImg.exe $FOLDER
+cp -rf ../data $FOLDER/data
+zip "$FOLDER.zip" -r $FOLDER
