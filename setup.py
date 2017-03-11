84550ff275af38321e1a445ee97c116dcee73fb6
Thibaut Mattio <thibaut.mattio@gmail.com>
2017-03-11 11:49:19 +0800

    Add Python 3.6 to the supported languages

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
     tests_require=test_requirements
