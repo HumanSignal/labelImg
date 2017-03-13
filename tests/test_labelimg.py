d3b1f2d8a1d2aeb63b2f25fb25d142c7f70a71a7
Thibaut Mattio <thibaut.mattio@gmail.com>
2017-03-13 12:06:38 +0800

    Change unit test python packaging import

--- tests/test_labelimg.py
+++ tests/test_labelimg.py
@@ -8,6 +8,11 @@ test_labelimg
 Tests for `labelimg` module.
 """
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from labelimg.__main__ import get_main_app
 
 
