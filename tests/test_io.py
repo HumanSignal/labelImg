bb4910a23b181d0d1316c416bad820982030cdef
Arnav Kohli <arnavkohli@gmail.com>
2017-05-03 16:27:58 +0800

    rename test files

--- tests/test_io.py
+++ tests/test_io.py
@@ -2,26 +2,8 @@ import _init_path
 
 from unittest import TestCase
 
-from labelImg import get_main_app
-
 from pascal_voc_io import PascalVocWriter
 from pascal_voc_io import PascalVocReader
-
-
-class TestMainWindow(TestCase):
-
-    app = None
-    win = None
-
-    def setUp(self):
-        self.app, self.win = get_main_app()
-
-    def tearDown(self):
-        self.win.close()
-        self.app.quit()
-
-    def test_noop(self):
-        pass
 
 # Test Write/Read
 writer = PascalVocWriter('tests', 'test', (512, 512, 1), localImgPath='tests/test.bmp')
