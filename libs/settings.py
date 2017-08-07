6545ceb1b6bf04e3107487f49e3faf63bfb8cd3a
tzutalin <tzu.ta.lin@gmail.com>
2017-08-08 05:10:34 +0800

    Refactor settings class

---
+++ libs/settings.py
@@ -0,0 +1,32 @@
+import pickle
+import os
+
+class Settings(object):
+    def __init__(self):
+        self.data = {}
+        self.path = '.settings.pkl'
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def get(self, key, default=None):
+        if key in self.data:
+            return self.data[key]
+        return default
+
+    def save(self):
+        with open(self.path, 'wb') as f:
+            pickle.dump(self.data, f, pickle.HIGHEST_PROTOCOL)
+            return True
+        return False
+
+    def load(self):
+        if os.path.exists(self.path):
+            with open(self.path, 'rb') as f:
+                self.data = pickle.load(f)
+                return True
+        return False
+
