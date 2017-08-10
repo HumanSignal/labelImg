f8d7b0d2310ce0b4ea9c85b5d53d8f2543c57e53
tzutalin <tzu.ta.lin@gmail.com>
2017-08-10 19:25:01 +0800

    Update run-in-container.sh

--- build-tools/run-in-container.sh
+++ build-tools/run-in-container.sh
@@ -1,4 +1,27 @@
-#!/bin/sh
+#!/bin/bash
+PS3='Select the supported images: '
+options=("py2qt4" "py3qt5" "Quit")
+IMAGE=tzutalin/py2qt4
+select opt in "${options[@]}"
+do
+    case $opt in
+        "py2qt4")
+            IMAGE=tzutalin/py2qt4
+            break
+            ;;
+        "py3qt5")
+            IMAGE=tzutalin/py3qt5
+            break
+            ;;
+        "Quit")
+            exit 0
+            ;;
+        *) echo invalid option;;
+    esac
+done
+
+echo $IMAGE
+
 docker run -it \
     --user $(id -u) \
     -e DISPLAY=unix$DISPLAY \
@@ -9,5 +32,5 @@ docker run -it \
     --volume="/etc/shadow:/etc/shadow:ro" \
     --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    tzutalin/py2qt4
+    $IMAGE
 
