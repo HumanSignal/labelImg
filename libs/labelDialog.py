5ca9a8fb655b1ebc81e9a31bf5190ea7441e9c8e
Arnav Kohli <arnavkohli@gmail.com>
2018-03-28 17:50:11 +0300

    add completer for label

--- libs/labelDialog.py
+++ libs/labelDialog.py
@@ -15,10 +15,18 @@ class LabelDialog(QDialog):
 
     def __init__(self, text="Enter object label", parent=None, listItem=None):
         super(LabelDialog, self).__init__(parent)
+
         self.edit = QLineEdit()
         self.edit.setText(text)
         self.edit.setValidator(labelValidator())
         self.edit.editingFinished.connect(self.postProcess)
+
+        model = QStringListModel()
+        model.setStringList(listItem)
+        completer = QCompleter()
+        completer.setModel(model)
+        self.edit.setCompleter(completer)
+
         layout = QVBoxLayout()
         layout.addWidget(self.edit)
         self.buttonBox = bb = BB(BB.Ok | BB.Cancel, Qt.Horizontal, self)
