b5632233c14eec7fbf6f9b77d2767e8f3145e6f9
Arnav Kohli <arnavkohli@gmail.com>
2018-04-03 21:39:29 +0200

    import unittest for line 26

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-from unittest import TestCase
-import time
-import sys
 import os
+import sys
+import time
+import unittest
+
 __author__ = 'TzuTaLin'
 
 dir_name = os.path.abspath(os.path.dirname(__file__))
@@ -10,7 +11,7 @@ libs_path = os.path.join(dir_name, '..', 'libs')
 sys.path.insert(0, libs_path)
 from settings import Settings
 
-class TestSettings(TestCase):
+class TestSettings(unittest.TestCase):
 
     def test_basic(self):
         wSetting = Settings()
