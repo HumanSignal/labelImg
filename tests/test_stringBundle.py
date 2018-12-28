7b5fd56c5467cc3ef9020c5089c40f186e6ae074
tzutalin <tzu.ta.lin@gmail.com>
2018-12-28 10:07:59 -0800

    fix the crash when providing invalid locale in env

--- tests/test_stringBundle.py
+++ tests/test_stringBundle.py
@@ -13,5 +13,16 @@ class TestStringBundle(unittest.TestCase):
         strBundle = StringBundle.getBundle('zh-TW')
         self.assertEqual(strBundle.getString("openDir"), u'\u958B\u555F\u76EE\u9304', 'Fail to load the zh-TW bundle')
 
+    def test_setInvaleLocaleToEnv_printErrorMsg(self):
+        prev_lc = os.environ['LC_ALL']
+        prev_lang = os.environ['LANG']
+        os.environ['LC_ALL'] = 'UTF-8'
+        os.environ['LANG'] = 'UTF-8'
+        strBundle = StringBundle.getBundle()
+        self.assertEqual(strBundle.getString("openDir"), 'Open Dir', 'Fail to load the default bundle')
+        os.environ['LC_ALL'] = prev_lc
+        os.environ['LANG'] = prev_lang
+
+
 if __name__ == '__main__':
     unittest.main()
