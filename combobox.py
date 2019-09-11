83f56ab6ebe379f022321560df7cf29fc741998f
Hakan Karaoguz <hakan.karaoguz@cybercom.com>
2019-09-11 23:36:20 +0200

    Update imports and added PyQt4 support

--- combobox.py
+++ combobox.py
@@ -1,7 +1,14 @@
-import sys
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+try:
+    from PyQt5.QtWidgets import QWidget, QHBoxLayout, QComboBox
+except ImportError:
+    # needed for py3+qt4
+    # Ref:
+    # http://pyqt.sourceforge.net/Docs/PyQt4/incompatible_apis.html
+    # http://stackoverflow.com/questions/21217399/pyqt4-qtcore-qvariant-object-instead-of-a-string
+    if sys.version_info.major >= 3:
+        import sip
+        sip.setapi('QVariant', 2)
+    from PyQt4.QtWidgets import QWidget, QHBoxLayout, QComboBox
 
 
 class ComboBox(QWidget):
@@ -12,12 +19,12 @@ class ComboBox(QWidget):
         self.cb = QComboBox()
         self.items = items
         self.cb.addItems(self.items)
-        
+
         self.cb.currentIndexChanged.connect(parent.comboSelectionChanged)
-            
+
         layout.addWidget(self.cb)
         self.setLayout(layout)
-        
+
     def update_items(self, items):
         self.items = items
 
