a0f2b1da16f41b78e0b73dd5ad2e2ade161002f3
WEN Hao <wenh06@gmail.com>
2021-05-10 11:54:41 +0800

    remove commented import of QtWebEngineWidgets

--- labelImg.py
+++ labelImg.py
@@ -18,7 +18,6 @@ try:
     from PyQt5.QtGui import *
     from PyQt5.QtCore import *
     from PyQt5.QtWidgets import *
-    # from PyQt5.QtWebEngineWidgets import *
 except ImportError:
     # needed for py3+qt4
     # Ref:
