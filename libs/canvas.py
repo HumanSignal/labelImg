7b3044d5787fbe957e8825c4122e6afbef368d6e
Zhang Ch. N <zhangchnxp@gmail.com>
2022-03-31 03:32:02 +0800

    Increase canvas epsilon

--- libs/canvas.py
+++ libs/canvas.py
@@ -31,7 +31,7 @@ class Canvas(QWidget):
 
     CREATE, EDIT = list(range(2))
 
-    epsilon = 11.0
+    epsilon = 24.0
 
     def __init__(self, *args, **kwargs):
         super(Canvas, self).__init__(*args, **kwargs)
